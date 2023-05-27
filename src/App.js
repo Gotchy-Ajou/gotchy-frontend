9299dcc02a7d1559b976ba287f75c8ec44e98cef
juho <jeff310@ajou.ac.kr>
2023-05-27 23:10:00 +0900

    apply page 5.27

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Layout/Header";
 import SideNavigation from "./components/Layout/SideNavigation";
 import { Col, Row } from "reactstrap";
-
 import { MainPage } from "./pages/MainPage";
 import Filter from "./pages/Filter";
+import ApplyPage from "./pages/ApplyPage";
 
 
 function App() {
@@ -18,25 +18,25 @@ function App() {
     },
   };
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Row>
-          <Col>
-            <Header></Header>
-          </Col>
-        </Row>
-        <div style={styles.contentDiv}>
-          <SideNavigation></SideNavigation>
-          <div style={styles.contentMargin}>
-          <Routes>
-            <Route path="/Filter" element={<Filter />} />
-            {/* <Route path="/members" component={Members} />
-          <Route path="/cashes" component={Cashes} /> */}
-          </Routes>
-          </div>
+    <BrowserRouter>
+    <Row>
+      <Col>
+        <Header></Header>
+      </Col>
+    </Row>
+    <div style={styles.contentDiv}>
+      <SideNavigation></SideNavigation>
+      <div style={styles.contentMargin}>
+      <Routes>
+        <Route path="/" element={MainPage} />
+        <Route path="ApplyPage" element={<ApplyPage />} />
+        <Route path="Filter" element={<Filter />} />
+        {/* <Route path="/members" component={Members} />
+        <Route path="/cashes" component={Cashes} /> */}
+      </Routes>
         </div>
-      </BrowserRouter >
-    </div >
+    </div>
+  </BrowserRouter>
   );
 }
 export default App;
--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,5 +1,14 @@
 import React from "react";
 import { AiOutlineUser } from "react-icons/ai";
+import ApplyPage from '../../pages/ApplyPage'
+import{
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  Switch
+}from 'react-router-dom';
+
 import {
   Nav,
   Navbar,
@@ -11,14 +20,14 @@ import {
 const Header = () => {
   return (
     <div>
-      <Navbar style={{"height": "100px"}} color="white" light expand="md">
-        <NavbarBrand style={{"font-size": "40px", "font-weight": "bold", "color": "#BD68B5"}} href="/">GOTCHY</NavbarBrand>
-        <Nav className="collapse navbar-collapse justify-content-end" navbar style={{"font-size": "20px"}}>
+      <Navbar style={{ "height": "100px" }} color="white" light expand="md">
+        <NavbarBrand style={{ "font-size": "40px", "font-weight": "bold", "color": "#BD68B5" }} href="/">GOTCHY</NavbarBrand>
+        <Nav className="collapse navbar-collapse justify-content-end" navbar style={{ "font-size": "20px" }}>
           <NavItem>
             <NavLink href="/Filter">가치 조회</NavLink>
           </NavItem>
           <NavItem>
-            <NavLink href="/ApplyPage">가치 신청</NavLink>
+            <NavLink tag={Link} to="/ApplyPage">가치 신청</NavLink>
           </NavItem>
           <NavItem>
             <NavLink href="/RegisterPage">가치 모집 등록</NavLink>
@@ -28,6 +37,10 @@ const Header = () => {
             <NavLink href="/InquiryPage">로그아웃</NavLink>
         </NavbarText> */}
       </Navbar>
+
+      <switch>
+        <Routes path="/ApplyPage" component={ApplyPage} />
+      </switch>
     </div>
   );
 };
