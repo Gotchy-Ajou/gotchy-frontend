381c35ae75fc2e18fde911a34f7cb514afb43edd
suji <qo9119@ajou.ac.kr>
2023-05-30 01:39:35 +0900

    가치 모집 등록 페이

---
+++ src/components/Reserve/index.jsx
@@ -0,0 +1,197 @@
+import styled from "styled-components";
+import { useState, useEffect } from "react";
+// import Dummy from "../../dummy.json";
+
+const Reserve = () => {
+  const today = new Date();
+  const TODAY = `${today.getFullYear()}-${String(today.getMonth() + 1).padStart(
+    2,
+    "0"
+  )}-${String(today.getDate()).padStart(2, "0")}`;
+
+  const hobbyList = [
+    "축구",
+    "농구",
+    "족구",
+    "탁구",
+    "배드민턴",
+    "테니스",
+    "합주(밴드, 오케스트라)",
+    "노래",
+    "춤",
+    "독서토론",
+    "보드게임",
+  ];
+  const location = [
+    "서울",
+    "경기",
+    "인천",
+    "강원",
+    "대전",
+    "충남/세종",
+    "충북",
+    "대구",
+    "경북",
+    "부산",
+    "울산",
+    "경남",
+    "광주",
+    "전남",
+    "전북",
+    "제주",
+  ];
+  const gender = ["남자", "여자"];
+  const level = ["비기너", "아마추어", "프로"];
+
+  const [data, setData] = useState({
+    gotchyId: `${Math.floor(Math.random() * 100000) + 100000}`,
+    location: "서울",
+    gotchyHobby: "축구",
+    gender: "남자",
+    level: "비기너",
+    mode: "Yes",
+    photoUrl: "",
+    price: null,
+    headcount: null,
+    useTime: null,
+    modifiedDate: TODAY,
+    createdDate: TODAY,
+    date: TODAY,
+  });
+
+  const onChangeData = ({ props, e }) => {
+    setData({ ...data, [props]: e.target.value });
+  };
+
+  return (
+    <>
+      <MainDiv>
+        <h1>모집 등록</h1>
+        <hr />
+        <InputContainer>
+          <select
+            onChange={(e) => onChangeData({ props: "gotchyHobby", e: e })}
+          >
+            {hobbyList.map((e) => (
+              <option>{e}</option>
+            ))}
+          </select>
+          <input
+            type={"date"}
+            onChange={(e) => onChangeData({ props: "date", e: e })}
+            value={data.date}
+          />
+          <select onChange={(e) => onChangeData({ props: "location", e: e })}>
+            {location.map((e) => (
+              <option>{e}</option>
+            ))}
+          </select>
+          <input
+            onChange={(e) => onChangeData({ props: "headcount", e: e })}
+            placeholder="인원 수를 입력해주세요."
+            type="number"
+            value={data.headcount}
+          />
+          <input
+            onChange={(e) => onChangeData({ props: "useTime", e: e })}
+            placeholder="이용시간을 입력해주세요."
+            type={"number"}
+            value={data.useTime}
+          />
+          <select onChange={(e) => onChangeData({ props: "gender", e: e })}>
+            {gender.map((e) => (
+              <option>{e}</option>
+            ))}
+          </select>
+          <select onChange={(e) => onChangeData({ props: "level", e: e })}>
+            {level.map((e) => (
+              <option>{e}</option>
+            ))}
+          </select>
+          <input
+            onChange={(e) => onChangeData({ props: "price", e: e })}
+            placeholder="보증금을 입력해주세요."
+            type="number"
+            value={data.price}
+          />
+          <div>
+            <button
+              onClick={() => {
+                if (Object.values(data).includes(null))
+                  alert("내용을 모두 입력해주세요..");
+                else {
+                  // api 요청.then (() => {
+                  alert("등록 되었습니다.");
+                  window.location.href = "/guide";
+                  //})
+                }
+              }}
+            >
+              가치 모집 등록
+            </button>
+          </div>
+        </InputContainer>
+      </MainDiv>
+    </>
+  );
+};
+export default Reserve;
+
+const InputContainer = styled.div`
+  margin-top: 30px;
+  width: 60%;
+  height: 80%;
+  border: 2px solid #8f23c0;
+  border-radius: 15px;
+  padding: 30px;
+  > div {
+    width: 100%;
+    display: flex;
+    justify-content: flex-end;
+    > button {
+      margin-top: 20px;
+      width: 250px;
+      height: 50px;
+      background-color: #8f23c0;
+      color: #fff;
+      font-size: 20px;
+      border: none;
+      border-radius: 10px;
+      cursor: pointer;
+    }
+  }
+  > input,
+  > select {
+    border: 2px solid #888;
+    width: 400px;
+    height: 7%;
+    border-radius: 5px;
+    padding-left: 10px;
+    margin-bottom: 15px;
+    display: flex;
+  }
+  > select {
+    width: 414px;
+  }
+`;
+
+const MainDiv = styled.div`
+  height: 100vh;
+  padding-top: 100px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  * {
+    outline: none;
+  }
+  > h1 {
+    margin: 0;
+  }
+  > hr {
+    background-color: #8f23c0;
+    border: none;
+    width: 150px;
+    height: 4px;
+  }
+`;
