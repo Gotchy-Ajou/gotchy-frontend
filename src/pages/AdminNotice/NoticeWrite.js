891efc38f8cfb083856179d0cea9a31dbdd6ff89
Soooobiniya <rorngk22@gmail.com>
2023-06-07 22:01:59 +0900

    왜 안돼

--- src/pages/AdminNotice/NoticeWrite.js
+++ src/pages/AdminNotice/NoticeWrite.js
@@ -34,7 +34,7 @@ function NoticeWrite() {
     function submitData() {
         console.log(data);
         axios.post("http://localhost:3000/api/v1/posts", {
-            'postId': data.postId,
+            'authonId': 10,
             'title': data.title,
             'content': data.content
         })
