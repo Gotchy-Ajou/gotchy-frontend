87202c4387b2f5166a912cdd48b5289c5be55b2c
Soooobiniya <rorngk22@gmail.com>
2023-06-07 21:11:41 +0900

    수정

--- src/pages/AdminNotice/NoticeWrite.js
+++ src/pages/AdminNotice/NoticeWrite.js
@@ -86,14 +86,14 @@ function NoticeWrite() {
                                 <tr>
                                     <th class="Write_header">제목</th>
                                     <td class="Write_input_container">
-                                        <input class="Write_input" type="text" value={title} onChange={onNoticeChange} size="50px" />
+                                        <input class="Write_input" type="text" name="title" value={title} onChange={onNoticeChange} size="50px" />
                                     </td>
                                 </tr>
 
                                 <tr>
                                     <th class="Write_header">내용</th>
                                     <td class="Write_input_container">
-                                        <textarea class="Write_input" value={content} onChange={onNoticeChange} rows="10"></textarea>
+                                        <textarea class="Write_input" name="content" value={content} onChange={onNoticeChange} rows="10"></textarea>
                                     </td>
                                 </tr>
 
